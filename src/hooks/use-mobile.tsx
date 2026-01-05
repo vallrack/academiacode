8055a7fc88612beb93ef7037576de9f3937f2b72
Danny Morales <programming2025dan@gmail.com>
2026-01-05 15:40:26 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,17 +3,16 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState<boolean>(false)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
+    const checkIsMobile = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    checkIsMobile()
+    window.addEventListener("resize", checkIsMobile)
+    return () => window.removeEventListener("resize", checkIsMobile)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
