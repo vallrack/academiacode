78a0b46ab4ad1ede8238e9d0eaa61ea5dc720e42
Danny Morales <programming2025dan@gmail.com>
2026-01-05 16:13:44 +0000

    mira la configuracion:
    // Import the functions you need from the SDKs y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,14 @@
+
+// The Firebase config object is built from environment variables.
+// This is a secure way to manage your Firebase credentials.
+
 export const firebaseConfig = {
-  "projectId": "studio-8025858435-569bf",
-  "appId": "1:89309975318:web:5c953fdd7e6e13bfceb637",
-  "apiKey": "AIzaSyBzKDBbPUVtGlHQdzmhmwOrvS2tBVbLlaY",
-  "authDomain": "studio-8025858435-569bf.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "89309975318"
+  apiKey: "AIzaSyB64Hi6l2uV8yvWBDH75qoNOSklsP7_BOk",
+  authDomain: "academiacode-f42d8.firebaseapp.com",
+  projectId: "academiacode-f42d8",
+  storageBucket: "academiacode-f42d8.appspot.com",
+  messagingSenderId: "466122254810",
+  appId: "1:466122254810:web:1af2fa59e104d11eabf8e3",
+  measurementId: "G-R2BC7GQQSV"
 };
+
