cb44e57ccf1c089e6b47ff038073d3059dfd1a9a
Danny Morales <programming2025dan@gmail.com>
2026-01-05 14:49:51 +0000

    no deja crear y ya habilite todo en firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,12 @@
+
 // The Firebase config object is provided by the server
 // and should not be modified.
 // See /src/firebase/index.ts for how it's used.
 export const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "AUTH_DOMAIN",
-  projectId: "PROJECT_ID",
-  storageBucket: "STORAGE_BUCKET",
-  messagingSenderId: "MESSAGING_SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyDP_3zT7c_axCoB1L_eB3B-o9v9R8yYy_A",
+  authDomain: "academiacode-2dcfd.firebaseapp.com",
+  projectId: "academiacode-2dcfd",
+  storageBucket: "academiacode-2dcfd.appspot.com",
+  messagingSenderId: "123161049246",
+  appId: "1:123161049246:web:13bda828a9b25ab1c3d0c2",
 };
