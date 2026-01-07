5cad6201cb4306b00e7d2140551023e162ebc642
Danny Morales <programming2025dan@gmail.com>
2026-01-07 14:36:34 +0000

    volvio el error de credenciales invalidas desde un navegador diferente a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
 
 // Es crucial que estas variables de entorno se configuren tanto en tu archivo .env.local como en Vercel.
 export const firebaseConfig = {
-  apiKey: "AIzaSyB64Hi6l2uV8yvWBDH75qoNOSklsP7_BOk",
-  authDomain: "academiacode-f42d8.firebaseapp.com",
-  projectId: "academiacode-f42d8",
-  storageBucket: "academiacode-f42d8.appspot.com",
-  messagingSenderId: "466122254810",
-  appId: "1:466122254810:web:1af2fa59e104d11eabf8e3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
+
