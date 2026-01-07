5c57a64163ecf3ce2db3781eb5e4154a9284aa85
Danny Morales <programming2025dan@gmail.com>
2026-01-07 13:02:51 +0000

    mira la configuracion:
    // Import the functions you need from the SDKs y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,14 +1,10 @@
 
-
-
 // Es crucial que estas variables de entorno se configuren tanto en tu archivo .env.local como en Vercel.
 export const firebaseConfig = {
-  apiKey: "AIzaSyBzKDBbPUVtGlHQdzmhmwOrvS2tBVbLlaY",
-  authDomain: "studio-8025858435-569bf.firebaseapp.com",
-  projectId: "studio-8025858435-569bf",
-  storageBucket: "studio-8025858435-569bf.appspot.com",
-  messagingSenderId: "89309975318",
-  appId: "1:89309975318:web:5c953fdd7e6e13bfceb637",
+  apiKey: "AIzaSyB64Hi6l2uV8yvWBDH75qoNOSklsP7_BOk",
+  authDomain: "academiacode-f42d8.firebaseapp.com",
+  projectId: "academiacode-f42d8",
+  storageBucket: "academiacode-f42d8.appspot.com",
+  messagingSenderId: "466122254810",
+  appId: "1:466122254810:web:1af2fa59e104d11eabf8e3"
 };
-
-
